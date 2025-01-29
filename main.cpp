d31b44d955d53464eb0b5744683c63db04f92dda
Arash Massoudieh <arashmassoudieh@gmail.com>
2025-01-28 20:54:24 -0500

    Network model working

--- main.cpp
+++ main.cpp
@@ -13,9 +13,14 @@ NetworkModel networkmodel(6);
 cout<<networkmodel.MaxDecimalRepresentation()<<endl;
 std::vector<std::vector<int>> matrix = networkmodel.createConnectivityMatrix(6, networkmodel.MaxDecimalRepresentation());
 
+System* system = new System(); 
+
 networkmodel.displayMatrix(matrix);
 
+ModelCreator m; 
+m.Create(system, &networkmodel);
 
+system->SavetoScriptFile("C:/Users/arash/Dropbox/Watershed_Modeling/sixnodemodel.ohq");
 #ifdef Behzad
     string Workingfolder="/home/behzad/Projects/ASM_Models/";
 #else
